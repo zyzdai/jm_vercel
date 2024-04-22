dfbe6327a673f694c691c2665f03eea5dea8c5dd
zyzdai <zyzdai@qq.com>
2024-04-22 19:54:41 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,8 @@
 import hashlib
-import os
-import uuid
 import requests
+from io import BytesIO
 from PIL import Image
-from flask import Flask, request, jsonify, make_response, send_from_directory
-
+from flask import Flask, request, jsonify, send_file
 
 app = Flask(__name__)
 def get_num(aid, index):
@@ -21,31 +19,23 @@ def get_num(aid, index):
     return normalCutNum
 
 def on_image_loaded(url):
-    # aid = 421536  # 漫画id
-    outfile_name = hashlib.md5(url.encode()).hexdigest() + '.jpg'
-    if  os.path.exists(outfile_name):
-        return outfile_name
     urls = url.split('/')
     aid = urls[-2]
     img_name = urls[-1]
-    img_path = f'{uuid.uuid4()}.jpg'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        with open(img_path, 'wb') as file:
-            file.write(response.content)
-        print(f"文件 {img_path} 下载成功")
-    else:
-        print(response.status_code,response.text)
+    if response.status_code != 200:
         print("下载失败")
-    img = Image.open(img_path)
+        return ""
+    img = Image.open(BytesIO(response.content))
     canvas = Image.new('RGB', img.size)  # 创建一个与图片大小相同的画布
     canvas.paste(img)  # 将图片粘贴到画布上
     index = img_name.split(".")[0]  # 获取图片在一组中的index，当前为00002
     cut_num = get_num(str(aid), str(index))  # 获取分割次数
     unknown = img.size[1] % cut_num  # 偏移高度，最后一张图的高度会比其他图高度要高
+    output_img = BytesIO()
     for m in range(cut_num):
         cut_height = img.size[1] // cut_num  # 分割的高度
         y_coordinate = cut_height * m  # 要分割的y坐标
@@ -57,23 +47,17 @@ def on_image_loaded(url):
         region = (0, end_coordinate, img.size[0], end_coordinate + cut_height)
         region_img = img.crop(region)
         canvas.paste(region_img, (0, y_coordinate))
-    canvas.save(outfile_name)  # 保存处理后的图片
-    # 删除img_path文件
-    os.remove(img_path)
-    return outfile_name
+    # 返回文件数据
+    canvas.save(output_img, format='JPEG')  # 这里可以根据需要选择图片格式
+    output_img.seek(0)
+    return output_img
 @app.route('/jm', methods=['GET', 'POST'])
 def go_jm():
     url = request.args.get('url')
     if len(url) <= 0:
         return jsonify({"code": "异常", "message": "url参数不能为空"})
-    outfile = on_image_loaded(url)
-    r = os.path.split(outfile)
-    try:
-        response = make_response(
-            send_from_directory(r[0], outfile, as_attachment=True))
-        return response
-    except Exception as e:
-        return jsonify({"code": "异常", "message": "{}".format(e)})
+    img_io = on_image_loaded(url)
+    return send_file(img_io, mimetype='image/jpeg', as_attachment=True)
 @app.route('/', methods=["GET"])
 def return_OneText():
     return "fina_res"
